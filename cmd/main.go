a269a1ffe978cd266321027487a5093051b16bdb
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2023-11-09 08:53:38 -0500

    start convert config file from json to yaml.

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,47 @@
 package main
 
 import (
-	"github.com/giuseppe-g-gelardi/git-sessionizer/auth"
-	"github.com/giuseppe-g-gelardi/git-sessionizer/cli"
+	"fmt"
+
+	// "github.com/giuseppe-g-gelardi/git-sessionizer/auth"
+	// "github.com/giuseppe-g-gelardi/git-sessionizer/cli"
 	c "github.com/giuseppe-g-gelardi/git-sessionizer/config"
 
-	"github.com/charmbracelet/log"
+	// "github.com/charmbracelet/log"
 )
 
 func main() {
-	// start the auth flow
-	// currently returns a boolean (isAuth true/false) and an error
-	_, err := auth.Authenticate() // auth.Authenticate()
-	if err != nil {
-		log.Errorf("Error: %v", err)
-	}
 
-	// instantiate the config manager and get the .configrc file
-	cm := c.NewConfigManager()
-	// currently returns a pointer to a UserConfig struct and an error
+	cm := c.NewCfgManager()
 	conf, err := cm.GetConfig(1)
 	if err != nil {
-		log.Errorf("Error: %v", err)
+		fmt.Println(err)
 	}
-
-	// should start the cli prompts
-	cli.InitCli(conf, cm)
+	fmt.Printf("%+v\n", conf)
 }
 
 // ! ===================================================================== ! //
 // ! ===================================================================== ! //
 // ! ===================================================================== ! //
 // ! ===================================================================== ! //
+
+
+// func main() {
+// 	// start the auth flow
+// 	// currently returns a boolean (isAuth true/false) and an error
+// 	_, err := auth.Authenticate() // auth.Authenticate()
+// 	if err != nil {
+// 		log.Errorf("Error: %v", err)
+// 	}
+
+// 	// instantiate the config manager and get the .configrc file
+// 	cm := c.NewConfigManager()
+// 	// currently returns a pointer to a UserConfig struct and an error
+// 	conf, err := cm.GetConfig(1)
+// 	if err != nil {
+// 		log.Errorf("Error: %v", err)
+// 	}
+
+// 	// should start the cli prompts
+// 	cli.InitCli(conf, cm)
+// }	
---
+++ config/cfg_yml.go
@@ -0,0 +1,95 @@
+package config
+
+import (
+	"errors"
+	"os"
+	"time"
+
+	"gopkg.in/yaml.v3"
+)
+
+type EditorCfg string
+
+const (
+	vsCode EditorCfg = "vscode"
+	vim    EditorCfg = "vim"
+	neovim EditorCfg = "nvim"
+)
+
+type Config struct {
+	AccessToken string    `json:"access_token" yaml:"access_token"`
+	Editor      EditorCfg `json:"editor" yaml:"editor"`
+	Alias       string    `json:"alias" yaml:"alias"`
+	Tmux        bool      `json:"tmux" yaml:"tmux"`
+}
+
+type CfgManager struct {
+	ConfigFileName string
+	DefaultConfig  Config
+}
+
+func NewCfgManager() *CfgManager {
+	return &CfgManager{
+		ConfigFileName: "config.yaml",
+		DefaultConfig: Config{
+			AccessToken: "",
+			Editor:      vsCode,
+			Alias:       "",
+			Tmux:        false,
+		},
+	}
+}
+
+func (cm *CfgManager) GetConfig(rcDepth int) (*Config, error) {
+	if rcDepth > 5 {
+		return nil, errors.New("unable to find config file")
+	}
+
+	configData, err := os.ReadFile(cm.ConfigFileName)
+	if err == nil {
+		var config Config
+		if err := yaml.Unmarshal(configData, &config); err != nil {
+			return nil, err
+		}
+		return &config, nil
+	}
+
+	// Error reading config file, making a new one
+	cm.WriteConfig(nil)
+	return cm.GetConfig(rcDepth + 1)
+}
+
+func (cm *CfgManager) WriteConfig(params interface{}) (*Config, error) {
+	var data []byte
+	if params != nil {
+		var err error
+		data, err = yaml.Marshal(params)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		var err error
+		data, err = yaml.Marshal(cm.DefaultConfig)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if err := os.WriteFile(cm.ConfigFileName, data, 0644); err != nil {
+		return nil, err
+	}
+
+	return cm.GetConfig(0)
+}
+
+func (cm *CfgManager) RevalidateConfig() error {
+	time.AfterFunc(2*time.Second, func() {
+		updatedConfig, err := cm.GetConfig(0)
+		if err != nil || updatedConfig.AccessToken == "" {
+			// Unable to update or verify config
+			cm.GetConfig(0)
+			return
+		}
+	})
+	return nil
+}
