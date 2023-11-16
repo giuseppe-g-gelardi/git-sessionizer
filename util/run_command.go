69ddeb08d9a27b812396ae48b504bfafd991c013
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2023-11-16 11:28:33 -0500

    add run command func

---
+++ util/run_command.go
@@ -0,0 +1,26 @@
+package util
+
+
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func RunCommand(command []string) error {
+    fmt.Printf(command[0], command[1:])
+
+
+	cmd := exec.Command(command[0], command[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+        fmt.Printf("Error running command: %v", command)
+		return err
+	}
+
+	return nil
+}
